1443a4ec9b40e90bd815c0c8d65c19e211d8aea0
chenxigao <819603673@qq.com>
2018-05-23 09:24:47 +0000

    finish static-pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title','Sample')-入门教程</title>
+</head>
+
+<body>
+{{--<header class="navbar navbar-fixed-top navbar-inverse">--}}
+    {{--<div class="col-md-offset-1 col-md-10">--}}
+        {{--<div>--}}
+
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</header>--}}
+@yield('content')
+
+</body>
+</html>
